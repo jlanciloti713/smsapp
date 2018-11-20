197853a26306a402aabdb5575811ae2d149a9837
Jack Lanciloti <jlanciloti713@gmail.com>
2018-11-19 19:51:33 -0600

    FEATURE:Add message responses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,11 +12,29 @@ class PagesController < ApplicationController
     incoming_message = params["Body"]
     message = "I AM A ROBOT"
 
-    if incoming_message == "Secret"
-      message = "You've unlocked a secret."
+    # if incoming_message == "Secret"
+    #   message = "You've unlocked a secret."
+    # else
+    #   message = "NORMALITY"
+    # end
+    case incoming_message
+    when "Secret"
+      message = "You've unlocked a secret"
+    when "PizzaTime"
+      message = "Spiderman 2"
+    when "Hey"
+      message = "Hello Sydney"
+    when "Abra"
+      message = "Kadabra"
     else
-      message = "NORMALITY"
+      message = "new phone who dis"
     end
+
+
+
+    
+      
+
     @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
     @client.messages.create(
       :from => twilio_phone_number,
