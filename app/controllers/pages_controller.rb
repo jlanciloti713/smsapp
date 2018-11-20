716b830a0233a46bf8ae912d2e881f4bc2266cf1
Jack Lanciloti <jlanciloti713@gmail.com>
2018-11-19 19:17:22 -0600

    FIX: Spelling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
     sender_number = params["From"]
     message = "Hello from Main Street Codes"
     @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
-    @client.message.creat(
+    @client.messages.create(
       :from => twilio_phone_number,
       :to => sender_number,
       :body => message
